b665d073f18c2a169305b7e4410cb533224f5b9a
Ben Hartshorne <ben@hound.sh>
2017-03-31 00:18:55 -0700

    upgrade to libhoney-go 1.3.0 with batching, fix default number of lines requested to a value that won't get you rate limited as easily

--- cli/cli.go
+++ cli/cli.go
@@ -21,7 +21,7 @@ type Options struct {
 	LogFile            string `short:"f" long:"log_file" description:"RDS log file to retrieve" default:"slowquery/mysql-slowquery.log"`
 	Download           bool   `short:"d" long:"download" description:"Download old logs instead of tailing the current log"`
 	DownloadDir        string `long:"download_dir" description:"directory in to which log files are downloaded" default:"./"`
-	NumLines           int64  `long:"num_lines" description:"number of lines to request at a time from AWS. Larger number will be more efficient, smaller number will allow for longer lines" default:"1000"`
+	NumLines           int64  `long:"num_lines" description:"number of lines to request at a time from AWS. Larger number will be more efficient, smaller number will allow for longer lines" default:"10000"`
 	BackoffTimer       int64  `long:"backoff_timer" description:"how many seconds to pause when rate limited by AWS." default:"5"`
 	Output             string `short:"o" long:"output" description:"output for the logs: stdout or honeycomb" default:"stdout"`
 	WriteKey           string `long:"writekey" description:"Team write key, when output is honeycomb"`
